4754ed7b28ea99231f0da86dd0cb6bb5548f0228
Richard Leurs <leursrichard@gmail.com>
2021-05-01 14:27:51 -0400

    Add subscribe button.

--- react/src/pages/podcast/index.js
+++ react/src/pages/podcast/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { getPodcastDetails } from "../../services/getService";
+import { getPodcastDetails, postSubscription } from "../../services/getService";
 
 const Podcast = () => {
     const { id } = useParams();
@@ -14,9 +14,16 @@ const Podcast = () => {
         getData();
     }, [id])
 
+    const subscribe = async () => {
+        await postSubscription(id);
+    }
+
     return (
         !podcast ? null :
-        <h1>{podcast.trackName}</h1>
+        <div className="flex justify-between">
+            <h1>{podcast.trackName}</h1>
+            <button class="button" onClick={subscribe}>Subscribe</button>
+        </div>
     )
 }
 
